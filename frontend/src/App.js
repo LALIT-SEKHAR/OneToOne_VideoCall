e5692170f81198b2e360c5cc97f9c6fc6f2357b4
LALIT-SEKHAR <lalitsekhar1999@gmail.com>
2021-08-12 14:50:44 +0530

    ice candidate create successfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,65 @@
+import { useEffect, useRef } from "react";
+import io from "socket.io-client";
 import "./App.css";
+import { v1 as uuid } from "uuid";
 
 function App() {
+  const socketRef = useRef();
+  const MyVideoRef = useRef();
+
+  // for getting the media streams
+  const getMediaStream = () => {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true, video: true })
+      .then((stream) => {
+        MyVideoRef.current.srcObject = stream;
+        window.mediaStream = stream;
+        RtcConnection();
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  //for RtcPeerConnection
+  const RtcConnection = () => {
+    const Ic = new RTCPeerConnection({
+      // iceServers: [
+      //   { urls: "stun:stun.stunprotocol.org" },
+      //   {
+      //     urls: "turn:numb.viagenie.ca",
+      //     credential: "muazkh",
+      //     username: "webrtc@live.com",
+      //   },
+      // ],
+    });
+    window.mediaStream.getTracks().forEach((track) => {
+      Ic.addTrack(track, window.mediaStream);
+    });
+    Ic.onicecandidate = (e) => {
+      // console.log("Ice Candidate: ", e);
+      // console.log(Ic.localDescription);
+      console.log("Ice Candidate");
+    };
+    Ic.ontrack = (e) => {
+      console.log("Track", e);
+    };
+    Ic.onnegotiationneeded = (e) => {
+      console.log("Negotiation End: ", e);
+    };
+    Ic.createOffer()
+      .then((offer) => Ic.setLocalDescription(offer))
+      .then(() => console.log("Offer set successfully"));
+  };
+
+  useEffect(() => {
+    getMediaStream();
+    socketRef.current = io.connect("/");
+    socketRef.current.emit("join room", uuid());
+  });
+
   return (
     <div className="App">
       <h1>WebRTC</h1>
+      <video className="videoTag" ref={MyVideoRef} muted autoPlay></video>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const socketio = require('socket.io');
-const server = require('http').createServer();
-const io = socketio(app())
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
+io.on("connection", (socket) => {
+  console.log("\x1b[43m\x1b[30m", " someone connected! ", "\x1b[0m");
+  socket.on("join room", (roomId) => {
+    console.log(roomId);
+  });
+});
 
-
-app.listen(8000)
+const PORT = 8000 || process.env.PORT;
+server.listen(PORT, () => {
+  console.log("\x1b[37m\x1b[44m", ` Listening at ${PORT} `, "\x1b[0m");
+});
