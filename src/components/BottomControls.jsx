627b7e91c9c22067400394b482c524a6e22ac290
LALIT-SEKHAR <lalitsekhar1999@gmail.com>
2021-08-18 17:58:06 +0530

    NEWFEATURE: add fullscreen toggle button

--- src/components/BottomControls.jsx
+++ src/components/BottomControls.jsx
@@ -4,18 +4,65 @@ import BC from "../css/BottomControls.module.css";
 const BottomControls = () => {
 
     const [IsMuted, setIsMuted] = useState(false)
+    const [IsFullScreen, setIsFullScreen] = useState(false)
 
     const MuteToggle = () => {
         document.getElementById("ClintVideoTag").muted = !document.getElementById("ClintVideoTag").muted
         setIsMuted(!document.getElementById("ClintVideoTag").muted)
     }
-    return (
-        <div className={BC.contender}>
-            <span className={BC.btn} onClick={MuteToggle}>
+    const FullScreenToggle = () => {
+        const section = document.getElementById("root");
+        var isInFullScreen =
+        (document.fullscreenElement && document.fullscreenElement !== null) ||
+        (document.webkitFullscreenElement &&
+            document.webkitFullscreenElement !== null) ||
+            (document.mozFullScreenElement &&
+                document.mozFullScreenElement !== null) ||
+                (document.msFullscreenElement && document.msFullscreenElement !== null);
+                if (!isInFullScreen) {
+                    setIsFullScreen(true)
+                    if (section.requestFullscreen) {
+                        section.requestFullscreen();
+                    } else if (section.mozRequestFullScreen) {
+                        section.mozRequestFullScreen();
+                    } else if (section.webkitRequestFullscreen) {
+                        section.webkitRequestFullscreen();
+                    } else if (section.msRequestFullscreen) {
+                        section.msRequestFullscreen();
+                    }
+                } else {
+                    setIsFullScreen(false)
+                    if (document.exitFullscreen) {
+                        document.exitFullscreen();
+                    } else if (document.webkitExitFullscreen) {
+                        document.webkitExitFullscreen();
+                    } else if (document.mozCancelFullScreen) {
+                        document.mozCancelFullScreen();
+                    } else if (document.msExitFullscreen) {
+                        document.msExitFullscreen();
+                    }
+                }
+            }
+            
+            const EndCall = () => {
+                window.Peer.close();
+                window.location.replace("/");
+            }
+            return (
+                <div className={BC.contender}>
+            <span onClick={MuteToggle}>
                 {IsMuted ?
                     <button className={BC.btn_original}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 9v6h4l5 5V4L7 9H3zm7-.17v6.34L7.83 13H5v-2h2.83L10 8.83zM16.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77 0-4.28-2.99-7.86-7-8.77z"/></svg></button>
                     :
                     <button className={BC.btn_danger}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4.34 2.93L2.93 4.34 7.29 8.7 7 9H3v6h4l5 5v-6.59l4.18 4.18c-.65.49-1.38.88-2.18 1.11v2.06c1.34-.3 2.57-.92 3.61-1.75l2.05 2.05 1.41-1.41L4.34 2.93zM10 15.17L7.83 13H5v-2h2.83l.88-.88L10 11.41v3.76zM19 12c0 .82-.15 1.61-.41 2.34l1.53 1.53c.56-1.17.88-2.48.88-3.87 0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zm-7-8l-1.88 1.88L12 7.76zm4.5 8c0-1.77-1.02-3.29-2.5-4.03v1.79l2.48 2.48c.01-.08.02-.16.02-.24z"/></svg></button>
+                }
+            </span>
+            <button onClick={EndCall} className={BC.btn_danger_medium}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M18.59 10.52c1.05.51 2.04 1.15 2.96 1.91l-1.07 1.07c-.58-.47-1.21-.89-1.88-1.27v-1.71m-13.2 0v1.7c-.65.37-1.28.79-1.87 1.27l-1.07-1.07c.91-.75 1.9-1.38 2.94-1.9M12 7C7.46 7 3.34 8.78.29 11.67c-.18.18-.29.43-.29.71s.11.53.29.7l2.48 2.48c.18.18.43.29.71.29.27 0 .52-.1.7-.28.79-.73 1.68-1.36 2.66-1.85.33-.16.56-.51.56-.9v-3.1C8.85 9.25 10.4 9 12 9s3.15.25 4.59.73v3.1c0 .4.23.74.56.9.98.49 1.88 1.11 2.67 1.85.18.17.43.28.7.28.28 0 .53-.11.71-.29l2.48-2.48c.18-.18.29-.43.29-.71s-.11-.53-.29-.71C20.66 8.78 16.54 7 12 7z"/></svg></button>
+            <span onClick={FullScreenToggle}>
+                {IsFullScreen ?
+                    <button className={BC.btn_danger}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"/></svg></button>
+                    :
+                    <button className={BC.btn_original}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/></svg></button>
                 }
             </span>
         </div>
