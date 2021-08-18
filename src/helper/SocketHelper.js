a507e00bfbaa7328d96e2f3730acb4767fa5a440
LALIT-SEKHAR <lalitsekhar1999@gmail.com>
2021-08-18 15:11:38 +0530

    LOGREMOVE: remove logs

--- src/helper/SocketHelper.js
+++ src/helper/SocketHelper.js
@@ -3,7 +3,6 @@ import { RtcReceive, RtcSetAnswer, RtcSetIce_candidate } from "./RtcPeerHelper";
 
 export const ServerCommunication = () => {
   return new Promise((resolve) => {
-    console.log(process.env.REACT_APP_SERVER_ENDPOINT);
     const socket = io.connect(process.env.REACT_APP_SERVER_ENDPOINT);
     socket.emit("join room", window.callId);
     socket.emit("user-two", (userId) => {});
